341a2ff128a0643a1e2dc1d0c86034e619feae38
Deve Tambat <54927248+tambatd@users.noreply.github.com>
2021-03-27 12:09:40 -0400

    Update square.js

--- square.js
+++ square.js
@@ -1,16 +1,29 @@
 function setup() {
-  frameRate(35);
-  createCanvas(1000, 1000);
+  frameRate(60);
+  createCanvas(1010, 1000);
   noStroke();
+  for(i=20;i<975;i+=150){
+  for(j=20;j<975;j+=50){
+textSize(32);
+text("Click Me!", i, j);
+  }
 }
 
-function draw() {
-  background(0);
-for(i=20;i<975;i+=25){
+}
+
+function draw(){
+  
+  
+
+
+}
+
+function mouseClicked(){
+  background(random(255), random(255), random(255));
+  for(i=20;i<975;i+=25){
   for(j=20;j<975;j+=20){
     fill(color(random(255), random(255),     random(255)));
     square(i, j, random(20)); 
   }
 }
-
 }
