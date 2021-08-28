4acc50baaa898eb9707a536584ef7c486399740d
Devendra <54927248+tambatd@users.noreply.github.com>
2021-08-28 14:04:14 -0400

    Update square.js

--- square.js
+++ square.js
@@ -1,5 +1,8 @@
+var fade = 0;
+var fadeAmount = 1
+
 function setup() {
-  createCanvas(1010, 1000);
+  createCanvas(windowWidth, windowHeight);
   noStroke();
   for (i = 20; i < 975; i += 150) {
     for (j = 20; j < 975; j += 50) {
@@ -9,14 +12,23 @@ function setup() {
   }
 }
 
-function draw() {}
+function draw() {
+  
+}
 
 function touchStarted() {
   background(random(255), random(255), random(255));
-  for (i = 20; i < 975; i += 25) {
-    for (j = 20; j < 975; j += 20) {
-      fill(color(random(255), random(255), random(255)));
+  for (i = 20; i < windowWidth - 25; i += 25) {
+    for (j = 20; j < windowHeight - 20; j += 20) {
+      fill(color(random(255), random(255), random(255), fade));
+      if (fade<0) fadeAmount=1; 
+ 
+  if (fade>255) fadeAmount=-10; 
+ 
+  fade += fadeAmount; 
       square(i, j, random(20));
     }
   }
 }
+
+
